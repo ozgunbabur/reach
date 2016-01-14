4ae30283f7ff0a012a2d29a2fc6d59947bf0ca5a
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-01-14 12:51:01 -0700

    Add input-output representations for mentions

---
+++ src/main/scala/edu/arizona/sista/assembly/IO.scala
@@ -0,0 +1,162 @@
+package edu.arizona.sista.assembly
+
+import edu.arizona.sista.odin.Mention
+import edu.arizona.sista.reach.mentions._
+
+
+abstract class IO {
+  def id: String
+  def mods: Set[String]
+  def text: String
+
+  // Customized equality
+  override def equals(o: Any) = o match {
+    // Check that the ID and mods are the same
+    // Could be input or output
+    case that: IO => that.id.equalsIgnoreCase(this.id) && that.mods.equals(this.mods)
+    case _ => false
+  }
+}
+
+case class Input(id: String, mods: Set[String], text: String) extends IO {
+
+  // Check to see if this Input is the Input of some Mention
+  // Attempts to use custom equality def of parent class (text doesn't need to match)
+  def isInputOf(m: Mention): Boolean = super.equals(IOResolver.getInput(m).get)
+  // Check to see if this Input is the Output of some Mention
+  // Attempts to use custom equality def of parent class (text doesn't need to match)
+  def isOutputOf(m: Mention): Boolean = super.equals(IOResolver.getOutput(m).get)
+
+  def isFuzzyOutputOf(m: Mention): Boolean = {
+    val out = IOResolver.getOutput(m).get
+    out match {
+      case something:Output =>
+        // approximate match on id OR text
+        this.id.equalsIgnoreCase(something.id) || this.text.equalsIgnoreCase(something.text)
+      case _ => false
+    }
+  }
+}
+
+case class Output(id: String, mods: Set[String], text: String) extends IO {
+  // Check to see if this Output is the Input of some Mention
+  // Attempts to use custom equality def of parent class
+  def isInputOf(m: Mention): Boolean = super.equals(IOResolver.getInput(m).get)
+  // Check to see if this Output is the Output of some Mention
+  def isOutputOf(m: Mention): Boolean = super.equals(IOResolver.getOutput(m).get)
+
+  def isFuzzyInputOf(m: Mention): Boolean = {
+    val input = IOResolver.getInput(m).get
+    input match {
+      case something:Input =>
+        // approximate match on id OR text
+        this.id.equalsIgnoreCase(something.id) || this.text.equalsIgnoreCase(something.text)
+      case _ => false
+    }
+  }
+}
+
+object IOResolver {
+
+  def getGroundingIDasString(m: Mention): String = m.toBioMention.xref.get.printString
+
+  // Get labels for PTM an Mutant mods
+  def getRelevantModifications(m: Mention): Set[String] =
+    m.toBioMention.modifications.flatMap {
+      case ptm: PTM => Set(ptm.label)
+      case mutant: Mutant => Set(mutant.label)
+      case _ => Set[String]()
+    }
+
+  // Represent the input to a Mention
+  // TODO: handle cases with multiple themes?
+  def getInput(mention: Mention): Option[Input] = mention match {
+
+    // Use the grounding ID of the TextBoundMention
+    // Should this only apply if btm.matches("Entity")
+    case btm: BioTextBoundMention =>
+      val id = btm.xref.get.printString
+      val text = btm.text
+      val mods = getRelevantModifications(btm)
+      Some(Input(id, mods, text))
+
+    // Is it an BioEventMention with a theme?
+    case bemWithTheme: BioEventMention if bemWithTheme.matches("SimpleEvent") && bemWithTheme.arguments.contains("theme") =>
+      // return the grounding id for the theme
+      val m = bemWithTheme.arguments("theme").head
+      val id = getGroundingIDasString(m)
+      val text = m.text
+      val mods =
+      // Get event label (PTM)
+        Set(bemWithTheme.label) ++
+          // Get relevant modifications
+          getRelevantModifications(m)
+      Some(Input(id, mods, text))
+
+    // Do we have a regulation?
+    case reg : BioRelationMention if reg.matches("ComplexEvent") && reg.arguments.contains("controller") && reg.arguments.contains("controlled") =>
+      val m = reg.arguments("controlled").head
+      val id = getGroundingIDasString(m)
+      val text = m.text
+      val mods =
+      // the label of the controlled (PTM?)
+        Set(m.label) ++
+          // the mods of the theme
+          getRelevantModifications(m) ++
+          // the mods of the theme of the theme
+          getRelevantModifications(m.arguments("theme").head)
+      Some(Input(id, mods, text))
+
+    // TODO: What is being left out?
+    case _ => None
+  }
+
+  // Represent the output of a Mention
+  // TODO: handle cases with multiple themes?
+  def getOutput(mention: Mention): Option[Output] = mention match {
+
+    // Use the grounding ID of the TextBoundMention
+    // Maybe this one should be removed?
+    // Should this only apply if btm.matches("Entity")
+    case btm: BioTextBoundMention => {
+      getInput(mention) match {
+        case input if input.nonEmpty =>
+          Some(Output(input.get.id, input.get.mods, input.get.text))
+        case _ => None
+      }
+    }
+
+    // Is it an BioEventMention with a theme?
+    case bemWithTheme: BioEventMention if bemWithTheme.matches("SimpleEvent") && bemWithTheme.arguments.contains("theme") =>
+      // return the grounding id for the theme
+      val m = bemWithTheme.arguments("theme").head
+      val id = getGroundingIDasString(m)
+      val text = m.text
+      val mods =
+      // Get event label (PTM)
+        Set(bemWithTheme.label) ++
+          // Get relevant modifications
+          getRelevantModifications(m)
+      Some(Output(id, mods, text))
+
+    // Do we have a regulation?
+    case reg : BioRelationMention if reg.matches("ComplexEvent") && reg.arguments.contains("controller") && reg.arguments.contains("controlled") =>
+      val m = reg.arguments("controlled").head
+      val id = getGroundingIDasString(m)
+      val text = m.text
+      val mods =
+      // the label of the controlled (PTM?)
+        Set(m.label) ++
+          // the mods of the theme
+          getRelevantModifications(m) ++
+          // the mods of the theme of the theme
+          getRelevantModifications(m.arguments("theme").head)
+      Some(Output(id, mods, text))
+
+    // TODO: What is being left out?
+    case _ => None
+  }
+
+  // Check to see if a Mention has output
+  def hasOutput(m: Mention): Boolean = getOutput(m).nonEmpty
+}
