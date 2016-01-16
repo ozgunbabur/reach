d60c38756053e5cd91107d9f6af490baaf704cd0
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-01-15 22:31:45 -0700

    Method for assembly RelM. display/debug

--- src/main/scala/edu/arizona/sista/assembly/SieveBasedAssembler.scala
+++ src/main/scala/edu/arizona/sista/assembly/SieveBasedAssembler.scala
@@ -14,6 +14,22 @@ class SieveBasedAssembler {
   // Take a seq of AssemblySieve and return a Seq[AssemblyGraph]
   def assemble(mentions:Seq[Mention], sieves:Seq[AssemblySieve]):Seq[AssemblyGraph] = sieves.map(_.assemble(mentions))
 
+  def displayAssembledMention(m: Mention):Unit = {
+    val before = m.arguments("before").head
+    val after = m.arguments("after").head
+    val input = if (IOResolver.hasOutput(before)) IOResolver.getOutput(before).get else None
+    val output = if (IOResolver.hasOutput(after)) IOResolver.getOutput(after).get else None
+    val text = s"${before.text} ... ${after.text}"
+    val representation =
+      s"""mention:           ${m.label}
+          |text:              $text
+          |output of before:  $input
+          |before label:      ${before.label}
+          |output of after:   $output
+          |after label:       ${after.label}
+          |""".stripMargin
+    println(representation)
+  }
 }
 
 object SieveBasedAssemblySystem extends App {
@@ -32,6 +48,7 @@ object SieveBasedAssemblySystem extends App {
   // read from config
   val serializeMentionsPath = config.getString("assembly.mentions.serializeto")
   val mentions:Seq[BioMention] =
+    // attempt to load the serialized mentions
     Try(MentionSerializer.load(serializeMentionsPath)) match {
       // did we successfully load the serialized mentions?
       case Success(preProcessedMentions) => preProcessedMentions.map(_.toBioMention)
